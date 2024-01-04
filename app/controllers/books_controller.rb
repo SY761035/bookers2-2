b1236292f2afbcb0c15edd16d89ce57357d20e37
EC2 Default User <ec2-user@ip-172-31-39-161.ap-northeast-1.compute.internal>
2024-01-04 12:58:50 +0000

    応用課題３　終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
     @book = Book.new
     @user = @book.user
     @show_book = Book.find(params[:id])
+    @book_comment = BookComment.new
   end
 
   def index
