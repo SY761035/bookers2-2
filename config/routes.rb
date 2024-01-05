bf9b29959b1d956fbf2b97347d43778cc271e41e
EC2 Default User <ec2-user@ip-172-31-39-161.ap-northeast-1.compute.internal>
2024-01-05 08:56:48 +0000

    応用課題4終了

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,16 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+  	get "followings" => "relationships#followings", as: "followings"
+  	get "followers" => "relationships#followers", as: "followers"
+  end
+ 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorite, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
-  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
