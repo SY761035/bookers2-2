cc69f77c7983620b06ccbf8f393e7e92045325ee
EC2 Default User <ec2-user@ip-172-31-39-161.ap-northeast-1.compute.internal>
2024-01-06 07:57:09 +0000

    応用課題５　終了

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     resource :favorite, only: [:create, :destroy]
   end
   
+  get "search", to: "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
